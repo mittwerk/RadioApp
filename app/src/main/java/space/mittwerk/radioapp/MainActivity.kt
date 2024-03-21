59c2e6a0c965a83f6482ca1be0f90119a92dae6c
WipedLifePotato <startyourday60@gmail.com>
2024-03-21 19:41:37 +0700

    Получать список стримов, с страницы

--- app/src/main/java/space/mittwerk/radioapp/MainActivity.kt
+++ app/src/main/java/space/mittwerk/radioapp/MainActivity.kt
@@ -1,25 +1,19 @@
 package space.mittwerk.radioapp
 
-import android.media.Image
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Close
-import androidx.compose.material.icons.sharp.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,20 +21,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import space.mittwerk.radioapp.ui.theme.RadioAppTheme
+import java.io.IOException
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread {
+            RadioStreams.initStreams()
+        }.start()
         setContent {
             RadioAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -57,13 +54,41 @@ class MainActivity : ComponentActivity() {
 }
 object RadioStreams
 {
+    fun initStreams() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://$domain:$port")
+            .build()
+        Log.d("HTML", "\"http://$domain:$port\"")
+        val html = try {
+            val response = client.newCall(request).execute()
+            if (!response.isSuccessful) {
+                throw IOException("Unexpected code $response")
+            }
+            response.body?.string() ?: throw IOException("Empty response body")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            ""
+        }
+        val regex = Regex("<h3 class=\"mount\">Mount Point \\/\\w+<\\/h3>")
+        val points = regex.findAll(html)
+        for(point in points)
+        {
+            val txt = point.value.split("/")[1].replace("<","")
+            //Log.d("POINT", txt)
+            streams.add(txt)
+        }
+        //Log.d("HTML", "HTML is now $html")
+        //Log.d("HTML", html)
+        //return mutableListOf("").toList()
+    }
     val domain = "[203:e93b:9902:9064:5efe:575b:3284:e1d2]"
     val port = 8000
-    val streams = listOf("synthwave", "psytrance", "stream.ogg")
-    val defaultStream = streams[0]
-    fun getStream(s: String? = null): String
+    val streams = mutableListOf<String>()
+
+    fun buildStreamLink(stream: String): String
     {
-        val stream = if(s == null) defaultStream else s
+        //val stream = s ?: defaultStream
         val fullLink = "http://${domain}:$port/$stream"
         Log.d("RadioStreams", "return $fullLink")
         return "http://${domain}:$port/$stream"
@@ -80,7 +105,7 @@ fun PlayStopButton(modifier: Modifier = Modifier) {
                 if (isPlayState) {
                     try {
                         mediaPlayer.apply {
-                            setDataSource(RadioStreams.getStream())
+                            setDataSource(RadioStreams.buildStreamLink(RadioStreams.streams[0]))
                             prepareAsync()
                             setOnPreparedListener {
                                 start()
